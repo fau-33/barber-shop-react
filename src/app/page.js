88ec2745e8d0af3bef6f4adafcd72ff42197e6ae
Flávio Félix <fau_felix30@hotmail.com>
2025-11-19 13:31:05 -0300

    Done struct Home

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,38 @@
+import estilos from "./page.module.css";
+import Banner from "../../public/banner.jpg";
+import Logo from "../../public/logo.png";
 import Image from "next/image";
-import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <h1>Home</h1>
+    <div>
+      <header className={estilos.header_container}>
+        <div>
+          <Image src={Logo} alt="logo" />
+          <button>Entrar</button>
+        </div>
+      </header>
+      <main className={estilos.main_container}>
+        <section className={estilos.secao_banner}>
+          <Image src={Banner} alt="banner" />
+        </section>
+        <section className={estilos.secao_texto}>
+          <div>
+            <h1>Bem-vindo a Barber Shop</h1>
+            <p>
+              Nossa barbearia sempre oferece profissionais de qualidade e
+              estamos prontos para lidar com suas maiores expectativas.
+            </p>
+            <p>
+              Nossos serviços são dedicados ao seu sucesso pessoal. Aqui temos
+              uma equipe premiada que demonstrou o talento de mestres barbeiros
+              em vários concursos de estilo. Deixe nosso barbeiro ser seu
+              estilista pessoal e você nunca ficará desapontado.
+            </p>
+            <p className={estilos.assinatura}>S. Kelly</p>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
